ee8c81d62d2addc8b0d3ffbc8af99bf91c63e8f1
Christian Teo <christeo2001@hotmail.com>
2024-04-08 02:00:08 -0400

    added more print statements in test to get more detailed results

--- src/test.rs
+++ src/test.rs
@@ -7,17 +7,17 @@ fn test_iterations() -> Result<(), Box<dyn std::error::Error>> {
 
   let mut arr: Vec<u32> = vec![];
 
-  for i in 0..100 {
-    arr.push(i);
+  for i in 0..1000000 {
+    arr.push(i*68);
   }
 
   let num = 14;
 
   let improved_results = algorithms::improved_binary_search(num, &arr);
-  println!("Improved iterations: {}", improved_results);
+  // println!("Improved iterations: {}", improved_results);
 
   let results = algorithms::binary_search(num, &arr, false);
-  println!("Iterations: {}", results);
+  // println!("Iterations: {}", results);
 
   Ok(())
 }
@@ -25,25 +25,33 @@ fn test_iterations() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn test_improved_binary_search() -> Result<(), Box<dyn std::error::Error>> {
 
-  let test_count = 1000;
-  let search_count = 1000;
+  let test_count = 10;
+  let search_count = 150;
 
   let mut improved_time_list: Vec<f32> = Vec::new();
   let mut improved_iterations: Vec<u32> = Vec::new();
   let mut time_list: Vec<f32> = Vec::new();
   let mut iterations: Vec<u32> = Vec::new();
+  let mut iteration_count = 0;
+  let mut search_list: Vec<u32> = Vec::new();
 
-  for _ in 0..test_count {
+  for test_num in 0..test_count {
 
-    let arr = algorithms::create_array();
+    println!("Test {}/{}", test_num+1, test_count);
+
+    // let arr = algorithms::create_random_array(100000000);
+    let arr = algorithms::create_step_array(100000000, 3);
     let len = arr.len();
     
     let min = arr[0];
     let max = arr[len-1];
 
-    for i in 0..search_count {
+    for search_num in 0..search_count {
 
-      let mut num = rand::random::<u32>() % len as u32;
+      println!("Search {}/{}", search_num+1, search_count);
+
+      let mut num = rand::random::<u32>() % max as u32;
+      search_list.push(num);
       
       if num < min {
         num *= min;
@@ -53,7 +61,7 @@ fn test_improved_binary_search() -> Result<(), Box<dyn std::error::Error>> {
         num %= max;
       }
 
-      num = min + 10;
+      // num = min + 10;
 
       let improved_start = Instant::now();
 
@@ -71,8 +79,13 @@ fn test_improved_binary_search() -> Result<(), Box<dyn std::error::Error>> {
       time_list.push(time);
       iterations.push(results);
 
-    }
+      if improved_result > results {
+        iteration_count += 1;
+      } else if improved_result < results {
+        iteration_count -= 1;
+      }
 
+    }
   }
 
   let improved_total_time: f32 = improved_time_list.iter().sum();
@@ -88,6 +101,17 @@ fn test_improved_binary_search() -> Result<(), Box<dyn std::error::Error>> {
   let average_iterations: f32 = total_iterations as f32 / (iterations.len() as f32);
   println!("Average time: {}", average_time);
   println!("Average iterations: {}", average_iterations);
+
+  if iteration_count < 0 {
+    println!("Improved binary search is better");
+  } else if iteration_count > 0 {
+    println!("Binary search is better");
+  } else {
+    println!("Both are equal");
+  }
+
+  println!("Iteration count: {}", iteration_count);
+  println!("Search list: {:?}", search_list);
 
   Ok(())
 }
